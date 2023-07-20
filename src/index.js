a2a2ad1853bf6473739f6861305dd31cf5368c8c
Esen Zheenchoroev <ezheenchoroev@kameleoon.com>
2023-07-20 17:39:47 +0300

    fix: proper cookie handling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as cookie from "cookie";
 
 const KAMELEOON_USER_ID = "kameleoon_user_id";
 
-export function handler(event, _, callback) {
+exports.handler = (event, _, callback) => {
   try {
     let request = null;
     let headers = {};
@@ -18,21 +18,28 @@ export function handler(event, _, callback) {
 
     let response = null;
     try {
-      request = event.Records[0].cf.response;
+      response = event.Records[0].cf.response;
     } catch (error) {
       console.log(
         "[KAMELEOON] WARNING: Unable to get response object from event."
       );
     }
 
+    console.log("REQUEST: ", JSON.stringify(request));
+    console.log("RESPONSE: ", JSON.stringify(response));
+
+    console.log("COOKIE: ", JSON.stringify(cookies));
+
     const parsedCookies =
-      cookies["cookie"] && cookies["cookie"][0]
+      cookies && cookies["cookie"] && cookies["cookie"][0]
         ? cookie.parse(cookies["cookie"][0].value)
         : {};
 
+    console.log("PARSEDCOOKIE: ", JSON.stringify(parsedCookies));
+
     const userId = parsedCookies[KAMELEOON_USER_ID] || "";
 
-    if (userId !== "") {
+    if (userId) {
       response.headers = {
         ...headers,
         "Set-Cookie": [
@@ -52,4 +59,4 @@ export function handler(event, _, callback) {
   } catch (error) {
     console.log(`[KAMELEOON]: Error generating viewer response: ${error}`);
   }
-}
+};
