bbb1e6f2ed256d4485573038213d28c37607f19e
Esen Zheenchoroev <ezheenchoroev@kameleoon.com>
2023-07-21 15:46:48 +0300

    fix: improve viewer request

--- src/viewer_request.js
+++ src/viewer_request.js
@@ -1,42 +1,65 @@
 import * as cookie from "cookie";
 import { KameleoonClient } from "@kameleoon/nodejs-sdk";
 import { getClientConfig, generateRandomUserId } from "./helpers";
+import { KAMELEOON_USER_ID } from "./constants";
 
-const KAMELEOON_USER_ID = "kameleoon_user_id";
 const KAMELEOON_SITE_CODE = "5gswtw0aep";
+
+/**
+ * Handler function is called when Lambda function is invoked.
+ * 1. User ID: Get the useId from cookie if it exists. Otherwise, generate a new userId.
+ * 2. Client Configuration: Get the client configuration data from cache if it exists. Otherwise, fetch fresh data from Kameleoon CDN.
+ * 3. Initialize KameleoonClient SDK: Create an instance of KameleoonClient using fetched client configuration.
+ * 4. Use kameleoonClient instance to access SDK methods. Using methdos get result for this particular userId.
+ * 5. Result: Return the result to the caller via appending headers or cookies to the callback function.
+ */
 
 exports.handler = async (event, _, callback) => {
   console.log("[KAMELEOON] Initializing function...");
 
   let request = null;
-  let headers = {};
-  let cookies = {};
 
   try {
     request = event.Records[0].cf.request;
-    headers = request.headers;
-    cookies = headers.cookie;
   } catch (error) {
-    console.log("[KAMELEOON] WARNING: Unable to get request or headers.");
+    console.log(
+      "[KAMELEOON] WARNING: Unable to get request object from event."
+    );
   }
+
+  let headers = {};
+  let cookieHeader = [];
+
+  try {
+    headers = request.headers;
+    cookieHeader = headers["cookie"] ?? [];
+  } catch (error) {
+    console.log(
+      "[KAMELEOON] WARNING: Unable to get headers object from request."
+    );
+  }
+
+  let userId = "";
 
   try {
     console.log("[KAMELEOON] Getting User ID...");
 
-    const parsedCookies =
-      cookies["cookie"] && cookies["cookie"][0]
-        ? cookie.parse(cookies["cookie"][0].value)
-        : {};
+    // 1. User ID: Get the useId from cookie if it exists. Otherwise, generate a new userId.
+    //    If userId is missing in request header cookie, then attach it to request header.
+    if (cookieHeader.length) {
+      const cookieValue = cookieHeader[0].value;
+      const cookies = cookie.parse(cookieValue);
 
-    let userId = parsedCookies[KAMELEOON_USER_ID] || "";
+      userId = cookies[KAMELEOON_USER_ID] || "";
+    }
 
-    if (userId === "") {
+    if (!cookieHeader.length || !userId) {
       userId = generateRandomUserId();
       headers = {
         ...headers,
-        "Set-Cookie": [
+        Cookie: [
           {
-            key: "Set-Cookie",
+            key: "Cookie",
             value: cookie.serialize(KAMELEOON_USER_ID, userId),
           },
         ],
@@ -46,22 +69,40 @@ exports.handler = async (event, _, callback) => {
     console.log(`[KAMELEOON] Using User ID: ${userId}`);
     console.log(`[KAMELEOON] Using site code: ${KAMELEOON_SITE_CODE}`);
 
+    // 2. Client Configuration: Get the client configuration data from cache if it exists. Otherwise, fetch fresh data from Kameleoon CDN.
     const clientConfig = await getClientConfig(KAMELEOON_SITE_CODE);
 
+    // 3. Initialize KameleoonClient SDK: Create an instance of KameleoonClient using fetched client configuration
     // const kameleoonClient = new KameleoonClient({
     //   siteCode: KAMELEOON_SITE_CODE,
     //   integrations: {
-    //     externalClientConfiguration: clientConfig
+    //     externalClientConfiguration: clientConfig,
     //   },
     // });
 
     // await kameleoonClient.initialize();
 
+    // 4. Use kameleoonClient instance to access SDK methods. You can refer to our developers documentation to find out more about methods
+    //    Here is the simple example of how to get the variationKey for this particular userId and feature flag.
     // const variationKey = kameleoonClient.getFeatureFlagVariationKey(
     //   userId,
     //   "test_fastly_starter_kit"
     // );
-    // console.log(`[KAMELEOON] The variationKey is ${variationKey}`);
+    // console.log(
+    //   `[KAMELEOON] The variationKey of userId: ${userId} is ${variationKey}`
+    // );
+
+    // 5. Result: Return the result to the caller via appending headers or cookies to the callback function.
+
+    // Example of attaching the result of `getFeatureFlagVariationKey` method to header to pass it a caller
+    // headers["<YOUR_FEATURE_KEY>"] = [
+    //   {
+    //     key: "<YOUR_FEATURE_KEY>",
+    //     value: variationKey,
+    //   },
+    // ];
+
+    // Here, you can include any other loficto handle the viewer request event.
 
     request = {
       ...request,
@@ -70,7 +111,7 @@ exports.handler = async (event, _, callback) => {
 
     callback(null, request);
   } catch (error) {
-    console.log(`[KAMELEOON] ERROR: ${error}`);
+    console.log(`[KAMELEOON] ERROR: generating viewer request: ${error}`);
     callback(null, request);
   }
 };
